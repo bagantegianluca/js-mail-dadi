a15ad1d9602579901c03d2991cb9477db77a3b7c
bagantegianluca <bagantegianluca@gmail.com>
2023-11-25 13:20:30 +0100

    Add pseudocode

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log('OK');
+/*
+Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+Consigli del giorno:
+scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+si ma noi cosa vogliamo fare?
+torniamo a scrivere in italiano
+proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+*/
+
+// Creo una variabile con una array della lista delle mail che possono accedere
+
+// Creo una variabile cui assegno un prompt per effettuare poi il controllo della mail inputata dall'utente
+
+// Effettuo controllo se la mail indicata dall'utente è autenticata all'accesso
+
+// Se autenticata creo una variabile per l'utente e per il computer per assegnare il numero casuale da 1 a 6 di ciascuno
+// Se il numero utente è maggiore del numero computer allora
+// Alert con messaggio vittoria utente, ricarica pagina per riprovare
+// Se invece il numero utente è minore del numero computer allora
+// Alert con messaggio sconfitta utente, ricarica pagina per riprovare
+// Altrimenti
+// Alert con messaggio pareggio, ricarica pagina per riprovare
+// Altrimenti
+// Alert non hai accesso al gioco, ricarica pagina per riprovare
